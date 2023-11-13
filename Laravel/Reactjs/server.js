bb22a6945b3a300807bc8e5e90816c617dcb4765
Harinderpal singh <hsbhular@gmail.com>
2023-11-13 20:15:43 +0530

    h

--- Laravel/Reactjs/server.js
+++
@@ -1,8 +0,0 @@
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
